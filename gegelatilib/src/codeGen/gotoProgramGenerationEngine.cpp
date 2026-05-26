99b3d2c3efbf3c02e80a522d6c62cb17f2cc9f4d
allpaul0 <paulallaireperso@gmail.com>
2026-05-26 14:37:11 +0200

    fix codegen exceeding quote

--- gegelatilib/src/codeGen/gotoProgramGenerationEngine.cpp
+++ gegelatilib/src/codeGen/gotoProgramGenerationEngine.cpp
@@ -116,7 +116,7 @@ void CodeGen::GotoProgramGenerationEngine::generateProgram(uint64_t progID,
     // -- Register array --
     int nbReg = static_cast<int>(
         this->program->getEnvironment().getParams().nbRegisters);
-    fileH << "\"t"
+    fileH << "\t"
     << this->dtype
     << " reg[" << nbReg << "] = {";
     for (int i = 0; i < nbReg; ++i) {
